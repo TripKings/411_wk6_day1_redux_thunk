14d5e9c08a59e3e206c6c05b2111be67a1c10a85
TripKings <antonio.cicchese@gmail.com>
2020-02-26 02:43:46 -0600

    thunk done

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,4 @@
-const url = "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json"
+// const url = "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json"
 
 export const addCar = (car) => {
     return {
@@ -13,3 +13,25 @@ export const removeCar = (index) => {
         value: index
     }
 }
+
+export const deleteMake = (index) => {
+    return {
+        type: 'DELETE_MAKE',
+        value: index
+    }
+}
+
+export const fetchMakes = () => {
+    return (dispatch) => {
+        fetch('https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json')
+            .then(res => res.json())
+            .then(response => {
+                const action = {
+                    type: 'FETCH_MAKES',
+                    value: response.Results
+                }
+                dispatch(action)
+            })
+    }
+}
+
